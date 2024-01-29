79d56722c01873aabeae69f366833401cb6951da
Cooper Griffin <coopergriffin@cmail.carleton.ca>
2024-01-29 18:41:40 -0500

    Updated index.js
    Added comments for future developer readability

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+//Cooper Griffin 
+//index file for project. 
+
+
+//npm declaration 
 const inquirer = require('inquirer');
 const fs = require('fs');
+//local libarary declarartion 
 const Triangle = require('./lib/Triangle');
 const Circle = require('./lib/Circle');
 const Square = require('./lib/Square');
 
+
+//Main/Starting function that get called 
 async function generateLogo() {
+
+  //User prompts. There are 4 prompts for the user
   const userInput = await inquirer.prompt([
     {
       type: 'input',
@@ -34,10 +44,10 @@ async function generateLogo() {
   shape.setColor(userInput.shapeColor);
 
   const svgContent = `<?xml version="1.0" encoding="UTF-8"?>
-<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
-  ${shape.render()}
-  <text x="150" y="100" font-size="16" fill="${userInput.textColor}" text-anchor="middle" dominant-baseline="middle">${userInput.text}</text>
-</svg>`;
+  		<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+  		${shape.render()}
+  		<text x="150" y="100" font-size="16" fill="${userInput.textColor}" text-anchor="middle" dominant-baseline="middle">${userInput.text}</text>
+		</svg>`;
 
   fs.writeFileSync('logo.svg', svgContent);
 
@@ -45,6 +55,7 @@ async function generateLogo() {
 }
 
 
+//Takes the users input for shape type prompt and call the corresponding shape function. 
 function getShape(shapeType) {
   switch (shapeType) {
     case 'circle':
